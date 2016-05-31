76a507307879dd041bc2a6b34da390f06f1c55b9
Steve Goldthorpe <steve@karmalb.org.uk>
2016-05-31 18:49:37 +0100

    add more build dependencies

--- setup-build-env.sh
+++ setup-build-env.sh
@@ -1,7 +1,9 @@
 REQPKGS="apt-file \
 	apt-utils \
+	autotools-dev \
 	checkinstall \
 	debhelper \
+	devscripts \
 	dh-make-perl \
 	dpkg-sig \
 	libemail-valid-perl \
@@ -12,15 +14,18 @@ REQPKGS="apt-file \
 	libnetaddr-ip-perl \
 	libnet-dns-perl \
 	libnet-ip-perl \
+	libpcre3-dev \
 	libreadonly-perl \
 	libssl-dev \
 	libtest-exception-perl \
 	lintian \
+	openssl \
 	packaging-dev \
 	quilt \
 	sudo \
 	wget \
 	xorriso"
+
 
 INSTALL=""
 for PKG in $REQPKGS; do
